5db965f89594b715b8d81626f33aa9f85d65d922
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-04-25 11:11:25 +0200

    Use official minecraft mappings for fabric & fix potential incompatibility issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,27 +2,27 @@
 
 ## [Unreleased]
 ### Changed
-
+- Fabric - Fixed potential incompatibility with other mods
 
 ## [1.16.5-2.2.2] - 2021-01-15
 ### Changed
-* Port to 1.16.5
+- Port to 1.16.5
 
 ## [1.16.4-2.2.2] - 2020-11-02
 ### Changed
-* Port to 1.16.4
+- Port to 1.16.4
 
 ## [1.16.3-2.2.2] - 2020-09-11
 ### Changed
-* Port to 1.16.3
+- Port to 1.16.3
 
 ## [1.16.2-2.2.2] - 2020-08-14
 ### Changed
-* Update pack format
+- Update pack format
 
 ## [1.16.2-2.2.1] - 2020-08-13
 ### Changed
-* Port to 1.16.2
+- Port to 1.16.2
 
 ## [1.16.1-2.0.0] - 2020-06-26
 ### Changed
--- fabric/src/main/java/fr/raksrinana/overpoweredmending/fabric/mixin/ExperienceOrbEntityMixin.java
+++ fabric/src/main/java/fr/raksrinana/overpoweredmending/fabric/mixin/ExperienceOrbEntityMixin.java
@@ -1,12 +1,12 @@
 package fr.raksrinana.overpoweredmending.fabric.mixin;
 
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.ExperienceOrbEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
+import net.minecraft.world.entity.ExperienceOrb;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,30 +14,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Comparator;
 import java.util.stream.IntStream;
 
-@Mixin(ExperienceOrbEntity.class)
+@Mixin(ExperienceOrb.class)
 public class ExperienceOrbEntityMixin{
 	private static final int DURABILITY_PER_XP = 2;
 	
-	@Inject(method = "onPlayerCollision", at = @At(value = "HEAD"), cancellable = true)
-	public void onPlayerCollision(PlayerEntity player, CallbackInfo callbackInfo){
-		ExperienceOrbEntity orb = (ExperienceOrbEntity) (Object) this;
+	@Inject(method = "playerTouch", at = @At(value = "HEAD"), cancellable = true)
+	public void onPlayerCollision(Player player, CallbackInfo callbackInfo){
+		ExperienceOrb orb = (ExperienceOrb) (Object) this;
 		
-		if (!orb.getEntityWorld().isClient()){
-			if(orb.pickupDelay == 0 && player.experiencePickUpDelay == 0){
-				player.experiencePickUpDelay = 2;
-				player.sendPickup(orb, 1);
+		if(!orb.getCommandSenderWorld().isClientSide()){
+			if(orb.throwTime == 0 && player.takeXpDelay == 0){
+				player.takeXpDelay = 2;
+				player.take(orb, 1);
 				
 				ItemStack item = getDamagedEnchantedItem(Enchantments.MENDING, player);
-				int xpAmount = orb.getExperienceAmount();
+				int xpAmount = orb.getValue();
 				
 				while(!item.isEmpty() && xpAmount > 0){
-					int realRepair = Math.min(xpAmount * DURABILITY_PER_XP, item.getDamage());
+					int realRepair = Math.min(xpAmount * DURABILITY_PER_XP, item.getDamageValue());
 					xpAmount -= realRepair / DURABILITY_PER_XP;
-					item.setDamage(item.getDamage() - realRepair);
+					item.setDamageValue(item.getDamageValue() - realRepair);
 					item = getDamagedEnchantedItem(Enchantments.MENDING, player);
 				}
 				if(xpAmount > 0){
-					player.addExperience(xpAmount);
+					player.giveExperiencePoints(xpAmount);
 				}
 				orb.remove();
 				callbackInfo.cancel();
@@ -45,15 +45,15 @@ public class ExperienceOrbEntityMixin{
 		}
 	}
 	
-	private static ItemStack getDamagedEnchantedItem(Enchantment ench, PlayerEntity player){
-		PlayerInventory playerInventory = player.inventory;
-		return IntStream.range(0, playerInventory.size())
-				.mapToObj(playerInventory::getStack)
+	private static ItemStack getDamagedEnchantedItem(Enchantment ench, Player player){
+		Inventory playerInventory = player.inventory;
+		return IntStream.range(0, playerInventory.getContainerSize())
+				.mapToObj(playerInventory::getItem)
 				.filter(is -> !is.isEmpty())
-				.filter(ItemStack::isDamageable)
+				.filter(ItemStack::isDamageableItem)
 				.filter(ItemStack::isDamaged)
-				.filter(is -> EnchantmentHelper.getLevel(ench, is) > 0)
-				.max(Comparator.comparing(ItemStack::getDamage))
+				.filter(is -> EnchantmentHelper.getItemEnchantmentLevel(ench, is) > 0)
+				.max(Comparator.comparing(ItemStack::getDamageValue))
 				.orElse(ItemStack.EMPTY);
 	}
 }
